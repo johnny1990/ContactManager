005258cfc080e24c68c604fddbb5dc10ba48168f
Man Ioan <man_ioan@live.com>
2020-12-27 10:38:33 +0200

    Add views for Contact & database script

---
+++ ContactManagerApplication/App_Data/DatabaseScript.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Addresses] (
+    [ID] INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    [City]      NVARCHAR (MAX) NULL,
+    [PostalCode]   NVARCHAR (10)     NOT NULL,
+    CONSTRAINT [PK_dbo.Addresses] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+CREATE TABLE [dbo].[Contacts] (
+    [ID]          INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (MAX) NULL,
+    [LastName]    NVARCHAR (MAX) NULL,
+    [DateOfBirth] DATETIME       NOT NULL,
+    [Email]       NVARCHAR (MAX) NULL,
+    [PhoneNumber] FLOAT (53)     NOT NULL,
+    [AddressID]   INT            NOT NULL,
+    CONSTRAINT [PK_dbo.Contacts] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_dbo.Contacts_dbo.Addresses_ID] FOREIGN KEY ([AddressID]) REFERENCES [dbo].[Addresses] ([ID]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ID]
+    ON [dbo].[Contacts]([ID] ASC);
+
---
+++ ContactManagerApplication/Controllers/ContactsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ContactManagerApplication.Models;
+
+namespace ContactManagerApplication.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private ContactsEntities db = new ContactsEntities();
+
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            var contacts = db.Contacts.Include(c => c.Address);
+            return View(contacts.ToList());
+        }
+
+        // GET: Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            ViewBag.AddressID = new SelectList(db.Addresses, "ID", "Name");
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,DateOfBirth,Email,PhoneNumber,AddressID")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AddressID = new SelectList(db.Addresses, "ID", "Name", contact.AddressID);
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AddressID = new SelectList(db.Addresses, "ID", "Name", contact.AddressID);
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,DateOfBirth,Email,PhoneNumber,AddressID")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AddressID = new SelectList(db.Addresses, "ID", "Name", contact.AddressID);
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
